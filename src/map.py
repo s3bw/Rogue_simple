3d4767d7a1a9c8d4c081abec97d6033230c4962c
Sebastien (Work) <sebastien@cytora.com>
2017-07-15 00:18:22 +0100

    initialised map and object testing habitat

--- src/map.py
+++ src/map.py
@@ -1,17 +1,43 @@
-
+# '.' and '@' need to be changed to True and False when GUI is added.
 
 class Tile:
-    def __init__(self, blocked=False):
+    def __init__(self, blocked='.'): #False):
         self.blocked = blocked
         
 class Map:
-    def __init__(self, map_h, map_v):
-        self.map_h = map_h
-        self.map_v = map_v
+    def __init__(self, grid_h, grid_v, grid=None):
+        self.grid_h = grid_h
+        self.grid_v = grid_v
+        self.grid = grid
         
-    def create_map(self):
-        map = [[
-            Tile(False)
-            for y in range(self.map_h)]
-                for x in range(self.map_v)
+        self.create_grid()
+        
+    def create_grid(self):
+        self.grid = [[
+            Tile('.')
+            for y in range(self.grid_h)]
+                for x in range(self.grid_v)
             ]
+
+    def show(self):
+        for x in self.grid:
+            print '| ' + ' | '.join([str(tile.blocked) for tile in x]) + ' |'
+            
+    def place_on_grid(self, x, y):
+        if self.grid[x][y].blocked == '.':
+            self.grid[x][y].blocked = '@'
+        
+    def remove_from_grid(self, x, y):
+        if self.grid[x][y].blocked == '@':
+            self.grid[x][y].blocked = '.'
+        
+        
+    def is_blocked(self, x, y):
+        if self.grid[x][y].blocked == '@':
+            return True
+            
+        # for objects in objects:
+            # if objects.blocks and object.x == x and object.y == y:
+                # return True
+                
+        return False
--- src/objects.py
+++ src/objects.py
@@ -1,15 +1,24 @@
-
+from src.map import Map
 
 
 class Object:
-    def __init__(self, x, y, name, creature=None)
+    def __init__(self, x, y, name, creature=None):
         self.x = x
         self.y = y
         self.name = name
         self.creature = creature
-    
+        
+        if self.creature:
+            self.creature.owner = self
+            
+    def move(self, grid, dx, dy):
+        if not grid.is_blocked(self.x + dx, self.y + dy):
+            self.x += dx
+            self.y += dy
+        
     
 class Creature:
     def __init__(self, hp):
         self.hp = hp
+
 
