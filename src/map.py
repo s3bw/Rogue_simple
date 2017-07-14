b8d0b34c8cb13a06c9fae122730e804f6c9e6d81
Sebastien (Work) <sebastien@cytora.com>
2017-07-14 21:55:26 +0100

    Addd initial map and object classes

--- src/map.py
+++ src/map.py
@@ -1,1 +1,17 @@
-print 'Map imported'
+
+
+class Tile:
+    def __init__(self, blocked=False):
+        self.blocked = blocked
+        
+class Map:
+    def __init__(self, map_h, map_v):
+        self.map_h = map_h
+        self.map_v = map_v
+        
+    def create_map(self):
+        map = [[
+            Tile(False)
+            for y in range(self.map_h)]
+                for x in range(self.map_v)
+            ]
--- src/objects.py
+++ src/objects.py
@@ -1,1 +1,15 @@
-print 'Objects imported'
+
+
+
+class Object:
+    def __init__(self, x, y, name, creature=None)
+        self.x = x
+        self.y = y
+        self.name = name
+        self.creature = creature
+    
+    
+class Creature:
+    def __init__(self, hp):
+        self.hp = hp
+
