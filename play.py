02caf31e87c3e0bdf7683e7317693a4b970079f6
Sebastien (Work) <sebastien@cytora.com>
2017-07-22 01:42:40 +0100

    Created items with basic functionality and drop on creature death

--- play.py
+++ play.py
@@ -1,35 +1,53 @@
 from src.map import Map
-from src.objects import Object_Place, Creature
+from src.objects import Object_Place, Creature, Item
 from src.objects import creature_death
+from src.containers import *
 # from src.message import Message
 
 
-moves = {'up': [-1,0],'down': [1,0],'left': [0,-1],'right': [0,1]}
+moves = {
+    'up': [-1,0],
+    'down': [1,0],
+    'left': [0,-1],
+    'right': [0,1]
+}
 
 map = Map(10, 10)
+WORLD_CONTAINER.append(map)
 
-all_objects = []
-
-player = Creature(50, 5, creature_death)
+player = Creature(hp=50, power=5, death=creature_death, inventory=[])
 user = Object_Place(5, 5, map, 'Player Character', '@', creature=player)
-all_objects.append(user)
+user_inventory = user.creature.inventory
+OBJECT_CONTAINER.append(user)
 
-rabbit_creature = Creature(10, 0, creature_death)
+stone_item = Item(weight=10, value=0)
+stone = Object_Place(None, None, map, 'Small Stone', 'o', item=stone_item)
+
+carrot_item = Item(weight=3, value=5)
+carrot = Object_Place(None, None, map, 'Carrot', 'v', item=carrot_item)
+
+rabbit_creature = Creature(hp=10, power=0, death=creature_death, inventory=[stone, carrot, stone])
 rabbit = Object_Place(3, 5, map, 'Rabbit', 'r', creature=rabbit_creature)
-all_objects.append(rabbit)
+OBJECT_CONTAINER.append(rabbit)
+
+user_inventory.append(stone)
+
+hat_item = Item(weight=2, value=10)
+hat = Object_Place(5, 3, map, 'Straw Hat', '^', item=hat_item)
+OBJECT_CONTAINER.append(hat)
 
 bin = Object_Place(7, 5, map, 'Bin', 'b')
-all_objects.append(bin)
+OBJECT_CONTAINER.append(bin)
 
 
 
 def render_map():
     map.refresh_grid()
-    for object in all_objects:
+    for object in OBJECT_CONTAINER:
         if object != user:
             object.draw()    
     
-        print (object.x, object.y)
+        print (object.x, object.y), object.name, object.representation
         
     user.draw()
     map.show()
@@ -42,7 +60,7 @@ def player_move(user_input):
     y = user.y + dy
     
     target = None
-    for object in all_objects:
+    for object in OBJECT_CONTAINER:
         if object.creature is not None and object.x == x and object.y ==y:
             target = object
             break
@@ -52,8 +70,32 @@ def player_move(user_input):
         
     else:
         user.move(dx, dy)
-         
-    
+        
+def player_drop():
+    if len(user_inventory) > 1:
+        print 'What item do you want to drop?'
+        for index, user_item in enumerate(user_inventory):
+            print '{}. {}'.format(index, user_item.name)
+        item_to_drop = raw_input('Item: ')
+        
+        for user_item in user_inventory:
+            if item_to_drop == user_item.name:
+                user_inventory.remove(user_item)
+                user_item.item.drop(user.x, user.y)
+                
+    elif user_inventory:
+        only_item = user_inventory[0]
+        user_inventory.remove(only_item)
+        only_item.item.drop(user.x, user.y)
+        
+def player_pick_up():
+    for object in OBJECT_CONTAINER:
+        if object != user and object.x == user.x and object.y == user.y and object.item:
+            user_inventory.append(object)
+            object.item.pick_up()
+            print 'Picked up {}.'.format(object.name)
+
+
 game_state = True
 while game_state == True:
     render_map()
@@ -62,8 +104,15 @@ while game_state == True:
     if user_input == 'exit':
         break
         
+    if user_input == 'drop':
+        player_drop()
+        
+    if user_input == 'pick up':
+        player_pick_up()
+        
     if user_input in moves:
-        player_move(user_input)    
+        player_move(user_input)
+        
 
 
 
--- src/objects.py
+++ src/objects.py
@@ -1,46 +1,58 @@
 from src.map import Map
+from src.containers import OBJECT_CONTAINER
 # from src.message import Message
 
 
 class Object_Place:
-    def __init__(self, x, y, grid_level, name, representation, passable=False, creature=None):
+    def __init__(self, 
+            x, y, grid_level, name, representation, passable=False,
+            creature=None,
+            item=None
+        ):
+        
         self.x = x
         self.y = y
         self.grid_level = grid_level
         self.name = name
         self.representation = representation
-        
         self.passable = passable
+        
         self.creature = creature
+        self.item = item
         
         if self.creature:
             self.creature.owner = self
+        
+        if self.item:
+            self.item.owner = self
+            self.passable = True
             
     def draw(self):
         self.grid_level.draw_on_grid(self.x, self.y, self.representation, self.passable)
             
-    # def send_to_back(self, object_list):
-        # object_list.remove(self)
-        # object_list.insert(0, self)
-        # return object_list
+    def send_to_back(self):
+        OBJECT_CONTAINER.remove(self)
+        OBJECT_CONTAINER.insert(0, self)
             
     def move(self, dx, dy):
         if not self.grid_level.is_blocked(self.x + dx, self.y + dy):
             self.x += dx
             self.y += dy
 
-        
     
 class Creature:
-    def __init__(self, hp, power, death):
+    def __init__(self, hp, power, death, inventory=None):
         self.hp = hp
         self.power = power
         self.death = death
+        self.inventory = inventory
         
     def take_damage(self, damage):
         if damage > 0:
             self.hp -= damage
             if self.hp <= 0:
+                # Maybe drop the item here.
+                # Item needs to be pickup-able, the corpse can't get in the way.
                 print '{} has Died.'.format(self.owner.name)
                 self.death(self.owner)
             else:
@@ -50,15 +62,37 @@ class Creature:
         damage = self.power
         if damage > 0:
             attacker = self.owner
-            print '{} has hit {} with {} worth of damage!'.format(attacker.name, target.name, damage)        
-            
+            print '{} has hit {} with {} worth of damage!'.format(attacker.name, target.name, damage)
             target.creature.take_damage(damage)
-
-        # Message.attack_message(attacker, target, damage, outcome)
             
+    def drop_item(self):
+        highest_value = max(self.inventory, key=lambda x: x.item.value)
+        self.inventory.remove(highest_value)
+        highest_value.item.drop(self.owner.x, self.owner.y)
+        
+        print 'Dropped {}.'.format(highest_value.name)
+
+class Item:
+    def __init__(self, weight, value):
+        self.weight = weight
+        self.value = value
+    
+    def pick_up(self):
+        OBJECT_CONTAINER.remove(self.owner)
+    
+    def drop(self, x, y):
+        self.owner.x = x
+        self.owner.y = y
+        OBJECT_CONTAINER.append(self.owner)
+        self.owner.send_to_back()
+        
+
 def creature_death(corpse):
+    corpse.creature.drop_item()
+    
     corpse.creature = None
     corpse.passable = True
     corpse.name = 'Mangled {} corpse.'.format(corpse.name)
     corpse.representation = '%'
+    corpse.send_to_back()
 
