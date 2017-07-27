f0451ec0d7a6affa725c7ad925e285db65179c4e
Sebastien (Work) <sebastien@cytora.com>
2017-07-27 14:43:18 +0100

    Finished all material dicts and starting to test distributions

--- src/data/materials.py
+++ src/data/materials.py
@@ -63,156 +63,219 @@
 # value, weight, rarity, stregth, special_attribute
 # higher value, more weight, high rarity, hard strength
 
+# I can add animal textiles 
 # I fell like the material globals should have less effect on the extremes (rarity)
 # I should do a random material comparisons to balance them after. e.g. jute vs beech (etc.)
 
+# For Consistant Ratings:
+# 
+# No mention of attribute == Average. 5 
+# Mention of edge case == 3 or 7 (e.g. light, heavy)
+# Mention of very case == 2 or 8 (e.g. very light, very heavy)
+# Mention of extreme edge case == 1 or 9 (e.g extremely heavy)
+# Mention of soft case == 4 or 6 (no very heavy)
+
 # 'wood': weight=4, rarity=3, stregth=4
-'oak', 5, 7, 5, 8
-'maple',6, 6, 6, 7, "shock_resistant"
-'mahogany',  8, 6, 7, 5, "varying strength"
-'cherry',7, 5, 6, 5
-'rosewood',8, 6, 7, 6, "dark"
-'teak',9, 8, 7, 7
-'ash',4, 8, 5, 5
-'pine',4, 3, 4, 4, "water resistant" 
-'hickory',4, 8, 4, 6
-'beech',5, 7, 5, 7
-'birch',9, 7, 7, 5
-'cedar',4, 4, 5, 3, "aromatic"
-'redwood',8, 7, 9, 6, "resistant to everything" 
-'hemlock',3, 4, 5, 7
-'fir',3, 5, 5, 3
-'spruce',3, 6, 3, 6
+# 'oak', 5, 7, 5, 8
+# 'maple',6, 6, 6, 7, "shock_resistant"
+# 'mahogany',  8, 6, 7, 5, "varying strength"
+# 'cherry',7, 5, 6, 5
+# 'rosewood',8, 6, 7, 6, "dark"
+# 'teak',9, 8, 7, 7
+# 'ash',4, 8, 5, 5
+# 'pine',4, 3, 4, 4, "water resistant" 
+# 'hickory',4, 8, 4, 6
+# 'beech',5, 7, 5, 7
+# 'birch',9, 7, 7, 5
+# 'cedar',4, 4, 5, 3, "aromatic"
+# 'redwood',8, 7, 9, 6, "resistant to everything" 
+# 'hemlock',3, 4, 5, 7
+# 'fir',3, 5, 5, 3
+# 'spruce',3, 6, 3, 6
 
 # 'textiles', weight=2, rarity=4, stregth=3
 # allows for macro level balacing
 
-    # 'cotton',7, 3, 6, 6, "many resistances" 
-    # 'silk',8, 2, 7, 6
-    # 'linen',5, 3, 5, 7
-    # 'wool',6, 5, 5, 8, "warm"
-    # 'leather',8, 6, 6, 8
-    # 'ramie',4, 4, 3, 7
-    # 'hemp',5, 4, 3, 6
-    # 'jute',6, 4, 6, 7, "helps carrying"
-
-{
-  "fir": {
-    "special": nan,
-    "strength": 3L,
-    "weight": 5L,
-    "value": 3L,
-    "rarity": 5L
-  },
-  "spruce": {
-    "special": nan,
-    "strength": 6L,
-    "weight": 6L,
-    "value": 3L,
-    "rarity": 3L
-  },
-  "hemlock": {
-    "special": nan,
-    "strength": 7L,
-    "weight": 4L,
-    "value": 3L,
-    "rarity": 5L
-  },
-  "redwood": {
-    "special": " resistant to everything ",
-    "strength": 6L,
-    "weight": 7L,
-    "value": 8L,
-    "rarity": 9L
-  },
-  "hickory": {
-    "special": nan,
-    "strength": 6L,
-    "weight": 8L,
-    "value": 4L,
-    "rarity": 4L
-  },
-  "cherry": {
-    "special": nan,
-    "strength": 5L,
-    "weight": 5L,
-    "value": 7L,
-    "rarity": 6L
-  },
-  "mahogany": {
-    "special": " varying strength",
-    "strength": 5L,
-    "weight": 6L,
-    "value": 8L,
-    "rarity": 7L
-  },
-  "oak": {
-    "special": nan,
-    "strength": 8L,
-    "weight": 7L,
-    "value": 5L,
-    "rarity": 5L
-  },
-  "pine": {
-    "special": " water resistant ",
-    "strength": 4L,
-    "weight": 3L,
-    "value": 4L,
-    "rarity": 4L
-  },
-  "teak": {
-    "special": nan,
-    "strength": 7L,
-    "weight": 8L,
-    "value": 9L,
-    "rarity": 7L
-  },
-  "cedar": {
-    "special": " aromatic",
-    "strength": 3L,
-    "weight": 4L,
-    "value": 4L,
-    "rarity": 5L
-  },
-  "birch": {
-    "special": nan,
-    "strength": 5L,
-    "weight": 7L,
-    "value": 9L,
-    "rarity": 7L
-  },
-  "beech": {
-    "special": nan,
-    "strength": 7L,
-    "weight": 7L,
-    "value": 5L,
-    "rarity": 5L
-  },
-  "rosewood": {
-    "special": " dark",
-    "strength": 6L,
-    "weight": 6L,
-    "value": 8L,
-    "rarity": 7L
-  },
-  "ash": {
-    "special": nan,
-    "strength": 5L,
-    "weight": 8L,
-    "value": 4L,
-    "rarity": 5L
-  },
-  "maple": {
-    "special": " shock_resistant",
-    "strength": 7L,
-    "weight": 6L,
-    "value": 6L,
-    "rarity": 6L
-  }
+# 'cotton',7, 3, 6, 6, "many resistances" 
+# 'silk',8, 2, 7, 6
+# 'linen',5, 3, 5, 7
+# 'wool',6, 5, 5, 8, "warm"
+# 'leather',8, 6, 6, 8
+# 'ramie',4, 4, 3, 7
+# 'hemp',5, 4, 3, 6
+# 'jute',6, 4, 6, 7, "helps carrying"
+    
+Textile_Data = {
+    "weight": 4,
+    "rarity": 3,
+    "strength": 4,
+    "textiles": {
+        "wool": {
+            "special": "warm",
+            "strength": 8,
+            "weight": 5,
+            "value": 6,
+            "rarity": 5
+        },
+        "leather": {
+            "strength": 8,
+            "weight": 6,
+            "value": 8,
+            "rarity": 6
+        },
+        "linen": {
+            "strength": 7,
+            "weight": 3,
+            "value": 5,
+            "rarity": 5
+        },
+        "ramie": {
+            "strength": 7,
+            "weight": 4,
+            "value": 4,
+            "rarity": 3
+        },
+        "hemp": {
+            "strength": 6,
+            "weight": 4,
+            "value": 5,
+            "rarity": 3
+        },
+        "silk": {
+            "strength": 6,
+            "weight": 2,
+            "value": 8,
+            "rarity": 7
+        },
+        "jute": {
+            "special": "helpscarrying",
+            "strength": 7,
+            "weight": 4,
+            "value": 6,
+            "rarity": 6
+        },
+        "cotton": {
+            "special": "manyresistances",
+            "strength": 6,
+            "weight": 3,
+            "value": 7,
+            "rarity": 6
+        }
+    }
+}
+    
+Wood_Data = {
+    "weight": 4,
+    "rarity": 3,
+    "strength": 4,
+    "woods": {
+        "fir": {
+            "strength": 3,
+            "weight": 5,
+            "value": 3,
+            "rarity": 5
+        },
+        "spruce": {
+            "strength": 6,
+            "weight": 6,
+            "value": 3,
+            "rarity": 3
+        },
+        "hemlock": {
+            "strength": 7,
+            "weight": 4,
+            "value": 3,
+            "rarity": 5
+        },
+        "redwood": {
+            "special": " resistant to everything ",
+            "strength": 6,
+            "weight": 7,
+            "value": 8,
+            "rarity": 9
+        },
+        "hickory": {
+            "strength": 6,
+            "weight": 8,
+            "value": 4,
+            "rarity": 4
+        },
+        "cherry": {
+            "strength": 5,
+            "weight": 5,
+            "value": 7,
+            "rarity": 6
+        },
+        "mahogany": {
+            "special": " varying strength",
+            "strength": 5,
+            "weight": 6,
+            "value": 8,
+            "rarity": 7
+        },
+        "oak": {
+            "strength": 8,
+            "weight": 7,
+            "value": 5,
+            "rarity": 5
+        },
+        "pine": {
+            "special": " water resistant ",
+            "strength": 4,
+            "weight": 3,
+            "value": 4,
+            "rarity": 4
+        },
+        "teak": {
+            "strength": 7,
+            "weight": 8,
+            "value": 9,
+            "rarity": 7
+        },
+        "cedar": {
+            "special": "aromatic",
+            "strength": 3,
+            "weight": 4,
+            "value": 4,
+            "rarity": 5
+        },
+        "birch": {
+            "strength": 5,
+            "weight": 7,
+            "value": 9,
+            "rarity": 7
+        },
+        "beech": {
+            "strength": 7,
+            "weight": 7,
+            "value": 5,
+            "rarity": 5
+        },
+        "rosewood": {
+            "special": " dark",
+            "strength": 6,
+            "weight": 6,
+            "value": 8,
+            "rarity": 7
+        },
+        "ash": {
+            "strength": 5,
+            "weight": 8,
+            "value": 4,
+            "rarity": 5
+        },
+        "maple": {
+            "special": " shock_resistant",
+            "strength": 7,
+            "weight": 6,
+            "value": 6,
+            "rarity": 6
+        }
+    }
 }
 
+
 Metal_Data = {
-    "metal": {
     "weight": 5,
     "rarity": 5,
     "strength": 5,
@@ -305,7 +368,6 @@ Metal_Data = {
             "weight": 6,
             "value": 7,
             "rarity": 6
-        }
         }
     }
 }
