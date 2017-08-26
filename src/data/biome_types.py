40c8d61ed0dc0768053bb75d23fafb57ed042a24
Sebastien (Work) <sebastien@cytora.com>
2017-08-26 15:06:48 +0100

    Make a database for biomes
    The initial script to carry the information on the biomes that will be
    entered into the game in the future, this includes structure types.

---
+++ src/data/biome_types.py
@@ -0,0 +1,14 @@
+BIOME_DATA = {
+    'village':[
+        'house',
+       # 'tree',
+       # 'shop',
+        'river',
+        'bridge',
+       # 'npc'
+    ],
+    'dungeon':[
+        'open_space',
+        'hidden_treasure'
+    ]
+}
